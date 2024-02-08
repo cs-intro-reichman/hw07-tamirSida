f8bc8d6cbb617deb0beae99a169824c776bd02ad
Tamir Sida <tamirsida@Tamirs-MacBook-Pro.local>
2024-02-08 15:42:19 +0200

    v1

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -11,30 +11,34 @@ public class HashTagTokenizer {
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
-
-		In in = new In(fileName);
-
-		// Your code here
-
+		In in = new In("dictionary.txt");
+			for(int i=0;i<3000;i++){
+				dictionary[i]=in.readLine();
+			}
 		return dictionary;
 	}
 
-	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+	public static boolean existInDictionary(String word, String[]dictionary) {
+		for(int i=0;i<3000;i++){
+			if (word.equals(dictionary[i])) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
-
+		String Lowhash=hashtag.toLowerCase();
 		// Base case: do nothing (return) if hashtag is an empty string.
-        if (hashtag.isEmpty()) {
-            return;
-        }
- 
-        int N = hashtag.length();
-
-        for (int i = 1; i <= N; i++) {
-		
-        }
+        if (Lowhash.isEmpty()) return;
+        for (int i = 1; i <= Lowhash.length(); i++) {
+			String sub = Lowhash.substring(0, i);
+			if (existInDictionary(sub, dictionary)) {
+				System.out.println(sub);
+				breakHashTag(Lowhash.substring(i), dictionary);
+				break;
     }
+}
+	}
 
 }
